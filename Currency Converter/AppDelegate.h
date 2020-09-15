7c202c5034d765483aaeb065ff72de0fe6dae5c8
Ameer Muhammed <ami.mullan@gmail.com>
2020-09-15 18:25:31 +0530

    Initial Commit

---
+++ Currency Converter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Currency Converter
+//
+//  Created by AmeerMuhammed on 9/15/20.
+//  Copyright © 2020 AmeerMuhammed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Currency Converter/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Currency Converter
+//
+//  Created by AmeerMuhammed on 9/15/20.
+//  Copyright © 2020 AmeerMuhammed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
